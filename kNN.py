b2cc782789d6264fc35840c6454ef3b07fe632d7
yoyo@home <1197993367@qq.com>
2017-08-14 15:59:23 +0800

    2-2完成使用k-邻近算法改进约会网站的配对效果案例

--- kNN.py
+++ kNN.py
@@ -36,3 +36,40 @@ def classify0(inX, dataSet, labels, k):
     # 3、排序，字典中最大的标签数则为测试向量的类别
     ans = sorted(countDict.items(), key=lambda x: x[1], reverse=True)
     return ans[0][0]  # 返回类别
+
+
+def file2matrix(filename):
+    """
+    文件数据转换为矩阵格式
+    :param filename: 文件名
+    :return: 返回数据矩阵，类别标签list
+    """
+    fr = open(filename)
+    mLines = fr.readlines()
+    mCount = len(mLines)
+    mat = np.zeros((mCount, 3))  # 建立一个零矩阵
+    mLabelList = []
+    index = 0
+
+    # 循环解析文件
+    for line in mLines:
+        line = line.strip().split('\t')  # 分割行
+        mat[index, :] = line[0:3]
+        index += 1
+        mLabelList.append(int(line[-1]))  # 要强转换为int，否则变为str
+    return mat, mLabelList
+
+
+def autoNum(dataSet):
+    """
+    归一化特征值
+    :param dataSet:数据集
+    :return:归一化后的二维array，每列的最大差值，每列最小值
+    """
+    minValue = dataSet.min(axis=0)  # 对每一列进行min
+    maxValue = dataSet.max(axis=0)
+    mRange = maxValue - minValue
+    m = dataSet.shape[0]    #数据行数
+    normDataSet = dataSet-np.tile(minValue,(m,1))
+    normDataSet = normDataSet/np.tile(mRange,(m,1)) #numpy中矩阵除法要用solve，这里就是普通的除法
+    return normDataSet,mRange,minValue
--- main.py
+++ main.py
@@ -2,12 +2,64 @@
 # -*- coding: UTF-8 -*-
 
 import kNN
+import matplotlib.pyplot as plt
 
 
 def test():
+    """
+    测试KNN算法
+    :return:
+    """
     group, labels = kNN.createDataSet()
     print kNN.classify0([0, 0], group, labels, 3)
 
 
+def showDatingTestData():
+    """
+    测试约会案例，文件数据转换成矩阵数据,使用10%数据作为测试集
+    :return:
+    """
+    mRatio = 0.1
+    mat, labels = kNN.file2matrix('datingTestSet2.txt')
+    normMat, mRange, mMin = kNN.autoNum(mat)  # 数据归一化
+    mCount = mat.shape[0]  # 数据行数
+    mTestCount = int(mRatio * mCount);  # 测试集数目
+    mError = 0  # 错误数
+    for i in range(mTestCount):
+        mResult = kNN.classify0(normMat[i, :], normMat[mTestCount:mCount, :], labels[mTestCount:mCount], 5)
+        if (mResult != labels[i]):
+            mError += 1
+    print 'The error rate is: %f' % (mError * 1.0 / mTestCount)
+    print 'The total test count is %d and the error count is %d' % (mTestCount, mError)
+
+
+def showDatingFigure(mat, labels):
+    """
+    显示无类别标签的散点图
+    :return:
+    """
+    fig = plt.figure()
+    ax = fig.add_subplot(111)  # 画布分为1x1，画在第1个上
+    ax.scatter(mat[:, 0], mat[:, 1], s=5.0 * kNN.np.array(labels), c=kNN.np.array(labels))
+    # plt.axis([-0.5, 25, -0.2, 2])
+    plt.show()
+
+
+def showDatingInput():
+    # 输入测试数据
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    mPercentGame = float(raw_input('the percentange of time spent playing vedio games:'))
+    mPercentMiles = float(raw_input('the miles earned every year:'))
+    mpercentIce = float(raw_input('the ice cream consumed per year:'))
+    testArray = [mPercentMiles, mPercentGame, mpercentIce]
+
+    mat, labels = kNN.file2matrix('datingTestSet2.txt')
+    normMat, mRange, mMin = kNN.autoNum(mat)
+    ansType = kNN.classify0((testArray - mMin) / mRange, normMat, labels, 5)
+    print 'This guy is mostly', resultList[int(ansType) - 1]
+
+
 if __name__ == '__main__':
-    test()
+    # test()
+    # showDatingTestData()
+    showDatingInput()
